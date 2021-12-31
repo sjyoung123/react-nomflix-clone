3982ce9d60746cfbbd36367a0ebc3d3750c61f34
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-31 23:12:16 +0900

    Update movie slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMovies } from "../api";
@@ -14,14 +16,14 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ bgphoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)),
-    url(${(props) => props.bgPhoto});
+    url(${(props) => props.bgphoto});
   background-size: cover;
 `;
 
@@ -35,11 +37,98 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -220px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgphoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  display: flex;
+  align-items: end;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  background-color: ${(props) => props.theme.black.lighter};
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  opacity: 0;
+`;
+
+const rowVariants: Variants = {
+  hidden: { x: window.innerWidth },
+  visible: { x: 0 },
+  exit: { x: -window.innerWidth },
+};
+
+const boxVariants: Variants = {
+  normal: { scale: 1 },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: { delay: 0.3, type: "tween" },
+  },
+};
+
+const infoVariants: Variants = {
+  hover: { opacity: 1, transition: { delay: 0.3, type: "tween" } },
+};
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMovies>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
+
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1; // -1 -> Banner
+      setIndex((prev) => (prev >= maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const onlyBackdropPath = (movies: IGetMovies) => {
+    const copy = { ...movies };
+    copy.results.forEach((movie, index) => {
+      if (movie.backdrop_path === null) {
+        return copy.results.splice(index, 1);
+      }
+    });
+    return copy;
+  };
 
   return (
     <>
@@ -49,11 +138,43 @@ function Home() {
         ) : (
           <>
             <Banner
-              bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+              onClick={increaseIndex}
+              bgphoto={makeImagePath(data?.results[0].backdrop_path || "")}
             >
               <Title>{data?.results[0].title}</Title>
               <Overview>{data?.results[0].overview}</Overview>
             </Banner>
+            <Slider>
+              <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                <Row
+                  variants={rowVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  key={index}
+                  transition={{ type: "tween", duration: 1 }}
+                >
+                  {data &&
+                    { ...onlyBackdropPath(data) }.results
+                      .slice(1)
+                      .slice(offset * index, offset * index + offset)
+                      .map((movie) => (
+                        <Box
+                          variants={boxVariants}
+                          initial="normal"
+                          whileHover="hover"
+                          transition={{ type: "tween" }}
+                          key={movie.id}
+                          bgphoto={makeImagePath(movie.backdrop_path, "w400")}
+                        >
+                          <Info variants={infoVariants}>
+                            <h4>{movie.title}</h4>
+                          </Info>
+                        </Box>
+                      ))}
+                </Row>
+              </AnimatePresence>
+            </Slider>
           </>
         )}
       </Wrapper>
